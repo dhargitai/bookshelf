d953da7381443e2ec75a996cf36d61b9ced5d26c
David Hargitai <hargitai.david@e-vista.hu>
2020-10-19 13:46:54 +0200

    Bootstrap extra credit 1

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,31 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
-import {Logo} from 'components/logo'
+import {Logo} from './components/logo'
+import {Dialog, DialogContent} from '@reach/dialog'
+import '@reach/dialog/styles.css'
+
+const PageState = {
+  default: 'default',
+  loginDialogOpen: 'loginDialogOpen',
+  registerDialogOpen: 'registerDialogOpen',
+}
 
 function App() {
+  // const [dialogType, setDialogType] = useState('login')
+  // const [isDialogOpen, setIsDialogOpen] = useState(false)
+  const [pageState, setPageState] = useState(PageState.default)
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh',
-        width: '100%',
-      }}
-    >
-      <Logo />
+    <div>
+      <Logo width="80" height="80" />
       <h1>Bookshelf</h1>
-      <div
-        style={{
-          justifyContent: 'space-between',
-          columnGap: '5px',
-          display: 'flex',
-        }}
-      >
-        <button onClick={() => alert('Login klikkd')}>Login</button>
-        <button onClick={() => alert('Redzsiszter klikkd')}>Register</button>
+      <div>
+        <button onClick={() => setPageState(PageState.loginDialogOpen)}>
+          Login
+        </button>
       </div>
+      <div>
+        <button onClick={() => setPageState(PageState.registerDialogOpen)}>
+          Register
+        </button>
+      </div>
+      {pageState === PageState.loginDialogOpen && (
+        <Dialog onDismiss={() => setPageState(PageState.default)}>
+          <p>Login</p>
+        </Dialog>
+      )}
+      {pageState === PageState.registerDialogOpen && (
+        <Dialog onDismiss={() => setPageState(PageState.default)}>
+          <p>Register</p>
+        </Dialog>
+      )}
     </div>
   )
 }
