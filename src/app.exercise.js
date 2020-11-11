bc0250efc10a4866aa13607e1ac566481496c044
David Hargitai <hargitai.david@e-vista.hu>
2020-11-11 15:13:59 +0100

    6. Authentication 2. extra credit

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -3,45 +3,60 @@ import {jsx} from '@emotion/core'
 
 import React from 'react'
 import * as auth from 'auth-provider'
-import {client} from 'utils/api-client'
+import {client} from './utils/api-client'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
+import {useAsync} from 'utils/hooks'
+import * as colors from './styles/colors'
+import {FullPageSpinner} from 'components/lib'
 
-function App() {
-  /* const [user, setUser] = React.useState(async () => {
-    const token = await auth.getToken()
-    if (token) {
-      return (await client('me', {token})).user
-    } else {
-      return null
-    }
-  }) */
-  const [user, setUser] = React.useState(null)
+async function getUser() {
+  let user = null
 
-  React.useEffect(() => {
-    if (user) {
-      return
-    }
-    ;(async () => {
-      const token = await auth.getToken()
-      if (token) {
-        const userResponse = await client('me', {token})
-        setUser(userResponse.user)
-      } else {
-        setUser(null)
-      }
-    })()
-  }, [user])
-
-  const login = form => auth.login(form).then(u => setUser(u))
-  const register = form => auth.register(form).then(u => setUser(u))
-  const logout = () => {
-    auth.logout()
-    setUser(null)
+  const token = await auth.getToken()
+  if (token) {
+    user = (await client('me', {token})).user
   }
 
-  return user ? (
-    <AuthenticatedApp user={user} logout={logout} />
+  return user
+}
+
+function App() {
+  const {isIdle, isLoading, isError, setData, error, data} = useAsync(null)
+
+  React.useEffect(() => {
+    getUser().then(u => setData(u))
+  }, [])
+
+  const login = form => auth.login(form).then(u => setData(u))
+  const register = form => auth.register(form).then(u => setData(u))
+  const logout = () => {
+    auth.logout()
+    setData(null)
+  }
+
+  if (isError) {
+    return (
+      <div
+        css={{
+          color: colors.danger,
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <p>Uh oh... There's a problem. Try refreshing the app.</p>
+        <pre>{error.message}</pre>
+      </div>
+    )
+  } else if (isIdle || isLoading) {
+    return <FullPageSpinner />
+  }
+
+  return data ? (
+    <AuthenticatedApp user={data} logout={logout} />
   ) : (
     <UnauthenticatedApp login={login} register={register} />
   )
